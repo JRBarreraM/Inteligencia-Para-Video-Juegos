ed8bcc9aab695c473f5aa79f691b7772f73d4510
JRBarreraM <35788695+JRBarreraM@users.noreply.github.com>
2018-10-09 07:40:51 -0400

    Align

---
+++ Inteligencia Para Video Juegos 1/Assets/Scripts/Align.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Align : MonoBehaviour {
+	public GameObject target;
+
+	void FixedUpdate () {
+
+		transform.rotation = Quaternion.RotateTowards(transform.rotation,target.transform.rotation,10f);
+	}
+}
