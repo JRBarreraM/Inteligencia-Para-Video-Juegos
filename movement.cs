c6bf25409d232c9e98ee366e21cc4f6dfdf49bca
José R. Barrera M <35788695+JRBarreraM@users.noreply.github.com>
2018-10-10 00:14:19 +0200

    Add files via upload

---
+++ movement.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movement : MonoBehaviour {
+
+	private float rottt = 0;
+
+	void Update () {
+
+		if (Input.GetKey (KeyCode.Z)) 
+		{
+			rottt += 10f;
+		}
+
+		if (Input.GetKey (KeyCode.M)) 
+		{
+			rottt -= 10f;
+		}
+
+		transform.rotation = Quaternion.Euler(0f,0f,transform.rotation.z + rottt);
+	}
+}
